f3d555e0d2f9b0e944d987786d92ee09a29e4760
MatillionPartnerEngineering <110067715+MatillionPartnerEngineering@users.noreply.github.com>
2024-08-19 16:18:17 -0600

    Update DIM_SHOE_SALES.sql

--- models/3-crossing-finish-line/DIM_SHOE_SALES.sql
+++ models/3-crossing-finish-line/DIM_SHOE_SALES.sql
@@ -11,7 +11,7 @@ SELECT
   when imdbrating between 8.5 and 9 then 'Priority watch'
   when imdbrating between 8 and 8.5 then 'Meh'
   when imdbrating <= 8 then 'Waste of time' end as watch_priority
-FROM {{source('netflix_search','SRC_NETFLIX_SEARCH')}}
+FROM {{source('CHARLIES_SHOE_EMPORIUM','STG_CHARLIES_SALES')}}
 
 {% if is_incremental() %}
 
