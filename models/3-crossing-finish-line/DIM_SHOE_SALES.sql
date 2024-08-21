c27e265498d3419a445cc815f83926758b13ceae
karey.graham@matillion.com <karey.graham@matillion.com>
2024-08-21 02:33:00 +0000

    crossing

--- models/3-crossing-finish-line/DIM_SHOE_SALES.sql
+++ models/3-crossing-finish-line/DIM_SHOE_SALES.sql
@@ -5,16 +5,14 @@
     )
 }}
 
-SELECT
-  *,
-  case when imdbrating >= 9 then 'Classic - must see'
-  when imdbrating between 8.5 and 9 then 'Priority watch'
-  when imdbrating between 8 and 8.5 then 'Meh'
-  when imdbrating <= 8 then 'Waste of time' end as watch_priority
-FROM {{source('CHARLIES_SHOE_EMPORIUM','STG_CHARLIES_SALES')}}
+SELECT * FROM {{ ref('STG_CHARLIES_SALES_CO') }}
+UNION ALL
+SELECT * FROM {{ ref('STG_CHARLIES_SALES_OR') }}
+UNION ALL
+SELECT * FROM {{ ref('STG_CHARLIES_SALES_FL') }}
 
 {% if is_incremental() %}
 
-WHERE ID not in (select ID from {{ this }})
+WHERE TRANSACTION_ID not in (select TRANSACTION_ID from {{ this }})
 
 {% endif %}
